2d34f76c32f654a4f0d558d9f0a220747e2f4154
Guillaume Laubier <glaubier@smartadserver.com>
2018-05-25 16:37:14 +0200

    bump cmp version to 4

--- smartcmp/src/main/java/com/smartadserver/android/smartcmp/Constants.java
+++ smartcmp/src/main/java/com/smartadserver/android/smartcmp/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
         // If you d'ont use it exactly as distributed in the official Smart AdServer repository,
         // you must get your own CMP ID by registering here: https://register.consensu.org/CMP
         public static final int ID = 33;
-        public static final int VERSION = 3;
+        public static final int VERSION = 4;
     }
 
     // IAB Keys for SharedPreferences storage.
